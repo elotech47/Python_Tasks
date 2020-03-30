697cc26c57261a458fb90ebb5fdb59489e02ad3b
elotech47 <eloghosaefficiency@gmail.com>
2020-03-30 16:21:01 +0100

    Complete code

--- python_task2_UserDataValidation.py
+++ python_task2_UserDataValidation.py
@@ -15,10 +15,23 @@ def DisplayDetails(Firstname, Lastname, email, password):
     print("Email: " + email + ".\n")
     print("password: " + password + ".\n")
 
+def DisplayStaffs():
+    print(":::::***********************************>>>")
+    print("The following are the registered employee..>>>\n")
+    k = 0
+    for i in employeeDetails:
+        print("Employee " + str(k+1) + "\n")
+        print("Firstname: " + i[0] )
+        print("Lastname: " + i[1] )
+        print("Email: " + i[2] )
+        print("password: " + i[3])
+        print("\n<<<------********------>>>")
+        k += 1
 
 #Employee detail container
 employeeDetails = []
 
+#Function to add Employee details
 def Add_employee():
     #Employee details
     Firstname = input("Please Enter your first name:_")
@@ -32,8 +45,12 @@ def Add_employee():
         Response = input("Are you statisfied with it?..y/n:>>")
         if Response == str("y" or "Y"):
             DisplayDetails(Firstname, Lastname, email, password) 
+            #Cache = {}
+            #Cache["Firstname","Lastname","Email","password"] = Firstname, Lastname, email, password
             currentEmployee = [Firstname, Lastname,email, password]   
             employeeDetails.append(currentEmployee)
+            break
+        
         elif Response == str("n" or "N"):
             UserPassword = input("Please Enter your desired Password[NB Password must not be less than 7 characters]:>>")
             while len(UserPassword) < 7:
@@ -43,20 +60,36 @@ def Add_employee():
                 DisplayDetails(Firstname, Lastname, email, password) 
                 currentEmployee = [Firstname, Lastname,email, password]   
                 employeeDetails.append(currentEmployee)
-        break
-    
-Add_employee()
-Resp = input("Add another employee?Y/N:>>")   
-if Resp == str("y" or "Y"):
-    Add_employee()
-else:
-    print("Ok thanks")
+            break
+        else:
+            print("Invalid Entry")
 
-print(employeeDetails)
+        
+
+Add_employee()
+#Loops to execute function until user enters "n" or "N"
+while True:
+    
+    Resp = input("Add another employee?Y/N:>>")   
+    if Resp == str("n" or "N"):
+        break
+    elif Resp == str("y" or "Y"):
+        Add_employee()
+        continue
+    else:
+        print("Invalid Entry")
+
+
+
+print("Ok thanks")
+
+DisplayStaffs()
 exit()
 
     #Employee details cache
 
+#for x, y in thisdict.items():
+ # print(x, y)
 
 
 
